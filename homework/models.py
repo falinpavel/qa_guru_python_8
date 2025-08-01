bf41838bfe3bc5ecf789f44b2f0de4883d92615f
Falin Pavel <falinpavel96@gmail.com>
2025-08-01 15:39:08 +0300

    hw done

--- homework/models.py
+++ homework/models.py
@@ -11,14 +11,6 @@ class Product:
     description: str
     quantity: int
 
-    # Пример обьекта:
-    # {
-    #     "name": "Milk",
-    #     "price": 100.0,
-    #     "description": "Milk from cow",
-    #     "quantity": 1000
-    # }
-
     # def __init__(self, name, price, description, quantity):
     #     self.name = name
     #     self.price = price
@@ -35,7 +27,6 @@ class Product:
         """
         TODO Верните True если количество продукта больше или равно запрашиваемому
             и False в обратном случае
-
         """
         return True if self.quantity >= quantity > 0 else False
 
@@ -51,6 +42,11 @@ class Product:
             self.quantity -= quantity
 
     def __hash__(self) -> int:
+        """
+        Метод __hash__ — это специальный метод в Python,
+        который позволяет определить как вычислять хеш
+        для вашего пользовательского объекта. Он возвращает целое число, хеш объекта.
+        """
         return hash((self.name, self.price, self.description, self.quantity))
 
 
@@ -62,17 +58,11 @@ class Cart:
     # Словарь продуктов и их количество в корзине
     products: dict[Product, int]
 
-    # Пример корзины для понимания
-    # {
-    #     Product("book1", 113, "This is a book1", 1000): 1,
-    #     Product("book2", 141, "This is a book2", 800): 2
-    # }
-
     def __init__(self):
         # По-умолчанию корзина пустая
         self.products = {}
 
-    def add_product(self, product: Product, buy_count=1) -> None:
+    def add_product(self, product: Product, buy_count: int = 1) -> None:
         """
         Метод добавления продукта в корзину.
         Если продукт уже есть в корзине, то увеличиваем количество
@@ -82,7 +72,7 @@ class Cart:
         else:
             self.products[product] = buy_count
 
-    def remove_product(self, product: Product, remove_count=None) -> None:
+    def remove_product(self, product: Product, remove_count: int = None) -> None:
         """
         Метод удаления продукта из корзины.
         Если remove_count не передан, то удаляется вся позиция
@@ -97,18 +87,35 @@ class Cart:
                 self.products[product] -= remove_count
 
     def clear(self) -> None:
-        self.products = {}
+        self.products.clear()
 
     def get_total_price(self) -> float:
         return sum([product.price * self.products[product] for product in self.products.keys()])
 
-    def buy(self, product: Product, quantity) -> None:
+    def buy_one(self, product: Product, quantity: int, money: int) -> None:
         """
         Метод покупки.
         Учтите, что товаров может не хватать на складе.
         В этом случае нужно выбросить исключение ValueError
         """
-        if Product.check_quantity(product, quantity) is False:
-            raise ValueError('Not enough products')
+        if money >= product.price:
+            if product.check_quantity(quantity) is False:
+                raise ValueError('Not enough products')
+            elif quantity == product.quantity:
+                self.remove_product(product=product)
+            else:
+                self.remove_product(product=product, remove_count=quantity)
+                self.products[product] -= quantity
         else:
+            raise ValueError('Not enough money')
+
+    def buy_all_cart(self, money: int) -> None:
+        """
+        Метод покупки.
+        Учтите, что товаров может не хватать на складе.
+        В этом случае нужно выбросить исключение ValueError
+        """
+        if money >= self.get_total_price():
             self.clear()
+        else:
+            raise ValueError('Not enough money')
--- homework/test_shop.py
+++ homework/test_shop.py
@@ -8,19 +8,16 @@ from homework.models import Product, Cart
 
 @pytest.fixture
 def product():
-    my_product = Product(
+    return Product(
         "book",
         100,
         "This is a book",
-        1000
-    )
-    return my_product
+        1000)
 
 
 @pytest.fixture
 def empty_cart():
-    cart = Cart()
-    return cart
+    return Cart()
 
 
 @pytest.fixture
@@ -69,12 +66,12 @@ class TestProducts:
     def test_product_buy_more_than_available(self, product):
         # TODO напишите проверки на метод buy,
         #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
-        assert product.quantity == 1000 # количество продуктов изначально
+        assert product.quantity == 1000
         with pytest.raises(ValueError, match='Not enough products'):
             product.buy(product.quantity + 1)
             product.buy(-1)
             product.buy(0)
-        assert product.quantity == 1000  # количество продуктов осталось неизменным
+        assert product.quantity == 1000
 
 
 class TestCart:
@@ -84,48 +81,101 @@ class TestCart:
         На некоторые методы у вас может быть несколько тестов.
         Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
+
     def test_cart_is_empty(self, empty_cart, product):
+        """
+        Тест кейс на проверку пустоты корзины
+        """
         assert empty_cart.products == {}
 
     def test_cart_add_product(self, empty_cart, product):
+        """
+        Тест кейс на добавление продукта в пустую корзину,
+        последующие проверки что товар добавился и его количество (проверяем дефолт by_count = 1)
+        """
         assert empty_cart.products == {}
-        empty_cart.add_product(product)
+        empty_cart.add_product(product=product)
         assert product in empty_cart.products
         assert empty_cart.products == {product: 1}
 
     def test_cart_add_and_remove_product(self, empty_cart, product):
+        """
+        Тест кейс на добавление продукта в пустую корзину,
+        последующие проверки что товар добавился и его количество,
+        затем удаление продукта из корзины методом remove_product
+        """
         assert empty_cart.products == {}
-        empty_cart.add_product(product, 5)
+        empty_cart.add_product(product=product, buy_count=5)
         assert product in empty_cart.products
         assert empty_cart.products == {product: 5}
-        empty_cart.remove_product(product, 1)
+        empty_cart.remove_product(product=product, remove_count=1)
         assert empty_cart.products == {product: 4}
-        empty_cart.remove_product(product)
+        empty_cart.remove_product(product=product)
         assert product not in empty_cart.products
         assert empty_cart.products == {}
 
     def test_remove_products_from_not_empty_cart(self, not_empty_cart, product):
+        """
+        Тест кейс на удаление продукта из не пустой корзины
+        и проверка исключения KeyError при удалении несуществующего продукта
+        """
         assert len(not_empty_cart.products) == 6
         with pytest.raises(KeyError):
-            not_empty_cart.remove_product(Product("test", 113, "test test test", 1000)) # товара нет в корзине
+            not_empty_cart.remove_product(Product("test", 113, "test test test", 1000))
 
     def test_cart_clear(self, not_empty_cart):
+        """
+        Тест кейс на очистку корзины и проверка пустоты
+        """
         assert not_empty_cart.products != {}
         not_empty_cart.clear()
         assert not_empty_cart.products == {}
 
     def test_get_total_price(self, not_empty_cart, product):
+        """
+        Тест кейс на получение общей стоимости корзины,
+        изменение стоимости корзины и проверка
+        очищения корзины и ее общей стоимости == 0
+        """
         assert not_empty_cart.get_total_price() == 6880
-        not_empty_cart.add_product(product)
+        not_empty_cart.add_product(product=product)
         assert not_empty_cart.get_total_price() == 6980
-        not_empty_cart.remove_product(product)
+        not_empty_cart.remove_product(product=product)
         assert not_empty_cart.get_total_price() == 6880
         not_empty_cart.clear()
         assert not_empty_cart.get_total_price() == 0
 
-    def test_buy(self, not_empty_cart):
+    def test_buy_one_product_in_cart(self, empty_cart, product):
+        """
+        Тест кейс на покупку одного продукта в корзине
+        с достаточным количеством денег и достаточным колличеством товаров
+        и проверка исключений ValueError
+        """
+        empty_cart.add_product(product, buy_count=1000)
+        assert product in empty_cart.products
+        assert empty_cart.products[product] == 1000
+        empty_cart.buy_one(product=product, quantity=empty_cart.products[product], money=100)
+        assert product not in empty_cart.products
+        assert empty_cart.products == {}
+        empty_cart.add_product(product, buy_count=1000)
+        with pytest.raises(ValueError):
+            empty_cart.buy_one(product=product, quantity=product.quantity, money=99)
+            empty_cart.buy_one(product=product, quantity=1001, money=150)
+            empty_cart.buy_one(product=product, quantity=-1, money=150)
+
+    def test_buy_more_then_one_product_in_cart(self, not_empty_cart):
+        """
+        Тест кейс на покупку всех продуктов в корзине,
+        проверки при попытке купить с недостаточным количеством денег
+        (проверка исключений ValueError)
+        и проверка очистки корзины при успешной покупке
+        """
+        assert len(not_empty_cart.products) == 6
         assert not_empty_cart.get_total_price() == 6880
-        all_products = not_empty_cart.products
-        for product in all_products:
-            not_empty_cart.buy(product, all_products[product])
+        with pytest.raises(ValueError):
+            not_empty_cart.buy_all_cart(6879)
+            not_empty_cart.buy_all_cart(0)
+            not_empty_cart.buy_all_cart(-1)
+        not_empty_cart.buy_all_cart(money=6880)
+        assert not_empty_cart.products == {}
         assert not_empty_cart.get_total_price() == 0
